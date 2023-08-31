98e028ef365b318e99a6f312d96f88bb94ff0718
lalindrait <57055160+lalindrait@users.noreply.github.com>
2023-08-31 20:53:24 +0530

    remove autocomplete for kubectl

--- vagrant/virtualbox/cluster-k8s-vms-3nodes/bootstrap_minimal.sh
+++ vagrant/virtualbox/cluster-k8s-vms-3nodes/bootstrap_minimal.sh
@@ -14,31 +14,3 @@ echo -e "root123\nroot123" | passwd root >/dev/null 2>&1
 echo "[TASK 10] Create user"
 useradd -p $(openssl passwd -1 k8s1234) k8s
 echo 'k8s        ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
-
-
-echo "[TASK 11] Enable auto-complete for kubectl"
-dnf install -y bash-completion
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
-source ~/.bashrc
-
-echo "[TASK 12] Configure git"
-dnf install -y git
-mkdir -p ~/projects/github-lalindrait
-cat >>~/.gitconfig<<EOF
-[user]
-        name = lalindra
-        email = lalindrait@gmail.com
-[alias]
-        st = status
-        a  = add .
-        cm = commit -m
-        pu = push
-        co = checkout
-        br = branch
-        lv = log --oneline
-        lvv = log --all --decorate --oneline --graph
-        lvvv = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'
-        lvvvv = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'
-EOF
